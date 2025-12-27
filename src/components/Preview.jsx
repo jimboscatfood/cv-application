307db8feacf470e33eaf351916bbc7562c13ccdb
jimboscatfood <149596389+jimboscatfood@users.noreply.github.com>
2025-12-27 16:58:45 +0800

    Added simple styles for the CV preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -4,10 +4,12 @@ export default function Preview({
   practicalInfoEntries,
 }) {
   return (
-    <div className="preview">
-      <Header generalInfoEntries={generalInfoEntries} />
-      <Education educationInfoEntries={educationInfoEntries} />
-      <Practical practicalInfoEntries={practicalInfoEntries} />
+    <div className="preview-container">
+      <div className="cv-preview">
+        <Header generalInfoEntries={generalInfoEntries} />
+        <Education educationInfoEntries={educationInfoEntries} />
+        <Practical practicalInfoEntries={practicalInfoEntries} />
+      </div>
     </div>
   );
 }
@@ -33,11 +35,12 @@ function Education({ educationInfoEntries }) {
       <h2>Educational Experience</h2>
       {educationInfoEntries.length > 0 &&
         educationInfoEntries.map((entry) => (
-          <div>
+          <div className="education-info">
             <h3 className="school">{entry.school}</h3>
             <h4 className="title">{entry.titleOfStudy}</h4>
-            <h5 className="date">{entry.startDate}</h5>
-            <h5 className="date">{entry.endDate}</h5>
+            <h5 className="start-date">{entry.startDate}</h5>
+            <span>-</span>
+            <h5 className="end-date">{entry.endDate}</h5>
           </div>
         ))}
     </div>
@@ -50,12 +53,13 @@ function Practical({ practicalInfoEntries }) {
       <h2>Practical Experience</h2>
       {practicalInfoEntries.length > 0 &&
         practicalInfoEntries.map((entry) => (
-          <div>
+          <div className="practical-info">
             <h3 className="company">{entry.company}</h3>
             <h4 className="position">{entry.position}</h4>
             <p className="responsibility">{entry.responsibility}</p>
-            <h5 className="date">{entry.startDate}</h5>
-            <h5 className="date">{entry.endDate}</h5>
+            <h5 className="start-date">{entry.startDate}</h5>
+            <span>-</span>
+            <h5 className="end-date">{entry.endDate}</h5>
           </div>
         ))}
     </div>
